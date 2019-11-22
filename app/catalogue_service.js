cb6c58fed5b52be7cedf2925049e66c4d2a0c4ff
jyoti <jyoti.contact@gmail.com>
2019-11-22 12:06:10 +0000

    Complete exercises

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 const catalogue = [
-  "The Catcher in the Rye by J.D. Salinger",
+  "The Catcher in the Rye by J.D.Salinger",
   "Dracula by Bram Stoker",
   "Between the Assassinations by Aravind Adiga",
   "Wolf Hall by Hilary Mantel",
@@ -18,29 +18,65 @@ const catalogue = [
   "Oliver Twist by Charles Dickens",
   "Great Expectations by Charles Dickens",
   "The Blind Assassin by Margaret Atwood",
-  "Why Be Happy When You Could Be Normal? by Jeanette Winterson",
+  "Why Be Happy When You Could Be Normal ? by Jeanette Winterson",
   "The Origin of Species by Charles Darwin"
 ];
-
 function countBooks() {
   // Your code here
+  return catalogue.length
 }
-
 function checkBook(book) {
   // Your code here
+  for (let i = 0; i < catalogue.length; i++) {
+    if (catalogue[i] === book) {
+      return true
+    }
+  } return false
+}
+function countBooksByFirstLetter(letter) {
+  let count = 0
+  for (let i = 0; i < catalogue.length; i++) {
+    let word = catalogue[i]
+    if (word.charAt(0).toUpperCase() === letter.toUpperCase()) {
+      count++
+    }
+    // console.log(word.charAt(0))
+  }
+  return count
 }
 
-function countBooksByFirstLetter(letter) {
-  // Your code here
-}
+
 
 function countBooksByKeyword(keyword) {
   // Your code here
+  let count = 0
+  if (typeof (keyword) === "string") {
+    for (let i = 0; i < catalogue.length; i++) {
+      let string = catalogue[i].toUpperCase();
+      if (string.includes(keyword.toUpperCase())) {
+        count++
+      }
+    }
+    return count
+  } else {
+    return "This is not valid";
+  }
 }
+
+
 
 function getBooksByAuthor(author) {
   // Your code here
+  let bookAuthor = [];
+  for (let i = 0; i < catalogue.length; i++) {
+    let string = catalogue[i];
+    if (string.includes(author)) {
+      bookAuthor.push(string);
+    }
+  }
+  return bookAuthor;
 }
+
 
 module.exports = {
   countBooks,
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,9 +1,89 @@
 const catalogueService = require("../app/catalogue_service");
-
 describe("catalogueService", () => {
   describe("catalogueService.countBooks", () => {
     test("returns the number of books in the list", () => {
       expect(catalogueService.countBooks()).toBe(20);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.checkBook", () => {
+    test("returns true if the book exists in the list", () => {
+      expect(catalogueService.checkBook("The Blind Assassin by Margaret Atwood")).toBe(true);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.checkBook", () => {
+    test("returns false if the book does not exist in the list", () => {
+      expect(catalogueService.checkBook("50 Shades of Grey by E.L.James")).toBe(false);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns the number of books beginning with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("N")).toBe(1);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns the number of books beginning with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("t")).toBe(5);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns the number of books beginning with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("j")).toBe(0);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns how many titles match the given keyword", () => {
+      expect(catalogueService.countBooksByKeyword(true)).toBe("This is not valid");
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns how many titles match the given keyword", () => {
+      expect(catalogueService.countBooksByKeyword("notAWord")).toBe(0);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns how many titles match the given keyword", () => {
+      expect(catalogueService.countBooksByKeyword("Normal")).toBe(2);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns how many titles match the given keyword", () => {
+      expect(catalogueService.getBooksByAuthor("Charles Dickens")).toEqual(["A Tale of Two Cities by Charles Dickens",
+        "Oliver Twist by Charles Dickens",
+        "Great Expectations by Charles Dickens"]);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns how many titles match the given keyword", () => {
+      expect(catalogueService.getBooksByAuthor("Not an Author")).toEqual([]);
+    });
+  });
+});
+describe("catalogueService", () => {
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns how many titles match the given keyword", () => {
+      expect(catalogueService.getBooksByAuthor("Charles")).toEqual(["A Tale of Two Cities by Charles Dickens",
+        "Oliver Twist by Charles Dickens",
+        "Great Expectations by Charles Dickens", "The Origin of Species by Charles Darwin"]);
     });
   });
 });
